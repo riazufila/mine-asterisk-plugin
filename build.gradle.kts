c6cbee063c4226de5a7ae2d536229639162f76ad
riazufila <46749569+riazufila@users.noreply.github.com>
2024-07-15 13:44:17 +0800

    Add: Dependencies and plugins.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "net.mine-asterisk.mc"
@@ -7,4 +8,21 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    // Paper API.
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+
+    // Hibernate.
+    implementation("org.hibernate.orm:hibernate-core:6.5.2.Final")
+    implementation("org.hibernate.orm:hibernate-hikaricp:6.5.2.Final")
+
+    // HikariCP.
+    implementation("com.zaxxer:HikariCP:5.1.0")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
