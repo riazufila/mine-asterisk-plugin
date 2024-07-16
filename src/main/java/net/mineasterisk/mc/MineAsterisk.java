4649c1d7caf8df1ac6eb73c0fe0d666f4a5c6999
riazufila <46749569+riazufila@users.noreply.github.com>
2024-07-16 11:44:34 +0800

    Add: Placeholder.

---
+++ src/main/java/net/mineasterisk/mc/MineAsterisk.java
@@ -0,0 +1,10 @@
+package net.mineasterisk.mc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MineAsterisk extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Plugin enabled.");
+    }
+}
