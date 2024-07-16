cbd4ff0208fa974fe2962f551a15a374ff0f5f9f
riazufila <46749569+riazufila@users.noreply.github.com>
2024-07-16 18:07:43 +0800

    Add: Player and Guild model.

---
+++ src/main/java/net/mineasterisk/mc/model/GuildModel.java
@@ -0,0 +1,99 @@
+package net.mineasterisk.mc.model;
+
+import jakarta.persistence.*;
+import net.mineasterisk.mc.constant.GuildStatus;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "guild")
+public class GuildModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "DATETIME")
+    private Instant createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "created_by", referencedColumnName = "id", nullable = false)
+    private PlayerModel createdBy;
+
+    @Column(name = "name", unique = true, nullable = false, length = 10)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "owner", referencedColumnName = "id", nullable = false)
+    private PlayerModel owner;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private GuildStatus guildStatus;
+
+    @OneToMany(mappedBy = "guild")
+    private Set<PlayerModel> players = new HashSet<>();
+
+    public GuildModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public @NotNull Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(@NotNull Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public @NotNull PlayerModel getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(@NotNull PlayerModel createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public @NotNull String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    public @NotNull PlayerModel getOwner() {
+        return owner;
+    }
+
+    public void setOwner(@NotNull PlayerModel owner) {
+        this.owner = owner;
+    }
+
+    public @NotNull GuildStatus getGuildStatus() {
+        return guildStatus;
+    }
+
+    public void setGuildStatus(@NotNull GuildStatus guildStatus) {
+        this.guildStatus = guildStatus;
+    }
+
+    public @NotNull Set<PlayerModel> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(@NotNull Set<PlayerModel> players) {
+        for (PlayerModel player : players) {
+            player.setGuild(this);
+        }
+    }
+}
---
+++ src/main/java/net/mineasterisk/mc/model/PlayerModel.java
@@ -0,0 +1,58 @@
+package net.mineasterisk.mc.model;
+
+import jakarta.persistence.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity(name = "player")
+public class PlayerModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "DATETIME")
+    private Instant createdAt;
+
+    @Column(name = "uuid", unique = true, nullable = false)
+    private UUID uuid;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "guild_id")
+    private GuildModel guild;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public @NotNull Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(@NotNull Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public @NotNull UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(@NotNull UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public @Nullable GuildModel getGuild() {
+        return guild;
+    }
+
+    public void setGuild(@NotNull GuildModel guild) {
+        this.guild = guild;
+    }
+}
