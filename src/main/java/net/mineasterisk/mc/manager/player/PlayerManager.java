09988b4a6477e81199cd90508336d5e498fb6acc
riazufila <46749569+riazufila@users.noreply.github.com>
2024-08-07 23:13:06 +0800

    Update: Disallow login if player initialization fails.

--- src/main/java/net/mineasterisk/mc/manager/player/PlayerManager.java
+++ src/main/java/net/mineasterisk/mc/manager/player/PlayerManager.java
@@ -1,6 +1,8 @@
 package net.mineasterisk.mc.manager.player;
 
 import java.time.Instant;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.mineasterisk.mc.constant.attribute.PlayerAttribute;
 import net.mineasterisk.mc.model.PlayerModel;
 import net.mineasterisk.mc.repository.PlayerRepository;
@@ -8,12 +10,13 @@ import net.mineasterisk.mc.service.PlayerService;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
 import org.jetbrains.annotations.NotNull;
 
 public class PlayerManager implements Listener {
   @EventHandler
-  public void onPlayerJoin(final @NotNull PlayerJoinEvent event) {
+  public void onPlayerJoin(final @NotNull PlayerLoginEvent event) {
     Player performedBy = event.getPlayer();
     PlayerModel playerToAdd = new PlayerModel(Instant.now(), performedBy.getUniqueId(), null);
 
@@ -21,7 +24,16 @@ public class PlayerManager implements Listener {
         .thenAccept(
             player -> {
               if (player == null) {
-                PlayerService.add(performedBy, playerToAdd);
+                PlayerService.add(performedBy, playerToAdd)
+                    .thenAccept(
+                        addedPlayer -> {
+                          if (!addedPlayer) {
+                            event.disallow(
+                                Result.KICK_OTHER,
+                                Component.text("Encountered error while joining...")
+                                    .color(NamedTextColor.RED));
+                          }
+                        });
               }
             });
   }
