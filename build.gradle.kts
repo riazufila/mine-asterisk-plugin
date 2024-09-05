1dae9ac4b69475c5dd2301b300a3cec4bc8dce06
riazufila <46749569+riazufila@users.noreply.github.com>
2024-09-05 21:52:02 +0800

    Update: Dependencies version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("xyz.jpenilla.run-paper") version "2.3.0"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 group = "net.mine-asterisk.mc"
@@ -13,12 +13,12 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
     implementation("com.zaxxer:HikariCP:5.1.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
     testImplementation("org.mockito:mockito-core:5.13.0")
-    testImplementation("com.github.seeseemelk:MockBukkit-v1.21:3.118.1")
+    testImplementation("com.github.seeseemelk:MockBukkit-v1.21:3.119.3")
     testImplementation("com.mysql:mysql-connector-j:9.0.0")
 }
 
@@ -28,7 +28,7 @@ java {
 
 tasks {
     runServer {
-        minecraftVersion("1.21")
+        minecraftVersion("1.21.1")
     }
 
     test {
